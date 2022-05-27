92d07cc1f35d0f3c694e408e79016bf2e049433d
Thomas Timio <thomas@semifir.com>
2022-05-27 11:14:38 +0200

    ajout config store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todosReducer from './features/todoSlice';
 
 export default configureStore({
-    reducer: {},
+    reducer: {
+        todos: todosReducer
+    },
 })
